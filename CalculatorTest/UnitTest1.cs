2b4806511187041a29025ab14f6314ca481d383b
Q <nhatquang2106qh@gmail.com>
2024-06-17 13:33:09 +0700

    done memberdata test

--- CalculatorTest/UnitTest1.cs
+++ CalculatorTest/UnitTest1.cs
@@ -13,11 +13,19 @@ namespace CalculatorTest
         //    Assert.Equal(15,calculator.Add(value1, value2));
         //}
         [Theory]
-        [ClassData(typeof(CalculatorTestData))]
+        //[ClassData(typeof(CalculatorTestData))]
+        [MemberData(nameof(DataTest))]
         public void Add_TwosNumber_ReturnSumofThem(int number1, int number2, int except)
         {
             Calculator calculator = new Calculator();
             Assert.Equal(except, calculator.Add(number1, number2));
         }
+        public static IEnumerable<object[]> DataTest =>
+            new List<object[]>
+            {
+                new object[]{1,2,3},
+                new object[]{-1,2,1},
+                new object[]{-1,-2,-3},
+            };
     }
 }
